cbe18f2f3cc52c5f0c06ae9f55534e60b6dee8e3
bw-vbnm <bwang19@illinois.edu>
2015-11-25 14:28:00 -0500

    basic sliding on click

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
+var slideNext = function(images, index){
+	var num_images = images.length;
+	var nextIndex = (index+1) % num_images;
+	images[index].className = 'slider-image';
+	images[nextIndex].className = 'slider-image show';
+}
+
 
 /**
  *	Adds event listeners
+ *
+ *	TODO: more robust, less assumptions
  */
 var init = function() {
-	//var sliders = document.getElementsByClassName('sliders');
+	var sliders = document.getElementsByClassName('slider');
+	for(var sliderIndex = 0; sliderIndex < sliders.length; sliderIndex++) {
+		var slider = sliders[sliderIndex];
+		
+		var images = slider.children[0].gallery.children; // hardcoded
+		var nav = slider.children[1];
+
+		for(var i = 0; i < images.length; i++) {
+			(function(index) {
+				images[i].addEventListener('click',function() {
+					slideNext(images,index);
+				});
+			})(i);
+		}
+	}
 };
 
 init();
