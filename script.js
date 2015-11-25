d5e3d3be0b02bf8793de9fe0a23548b15688b266
bw-vbnm <bwang19@illinois.edu>
2015-11-25 14:58:47 -0500

    basic slider nav

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-var slideNext = function(images, index){
+var SLIDER_CLASS = 'slider';
+var IMAGE_CLASS = 'slider-image';
+var IMAGE_SHOW_CLASS = 'slider-image show';
+
+
+var slideTo = function(images, index) {
+	for(var i = 0; i < images.length; i++) {
+		images[i].className = IMAGE_CLASS;	
+	}
+	images[index].className = IMAGE_SHOW_CLASS;
+};
+
+var slideNext = function(images, index) {
 	var num_images = images.length;
 	var nextIndex = (index+1) % num_images;
-	images[index].className = 'slider-image';
-	images[nextIndex].className = 'slider-image show';
-}
+	images[index].className = IMAGE_CLASS;
+	images[nextIndex].className = IMAGE_SHOW_CLASS;
+};
 
 
 /**
@@ -15,15 +27,20 @@ var init = function() {
 	var sliders = document.getElementsByClassName('slider');
 	for(var sliderIndex = 0; sliderIndex < sliders.length; sliderIndex++) {
 		var slider = sliders[sliderIndex];
-		
-		var images = slider.children[0].gallery.children; // hardcoded
-		var nav = slider.children[1];
+		var images = slider.children[0].children; // hardcoded, FIX LATER
+		var nav = slider.children[1].children[0]; // hardcoded, FIX LATER
 
 		for(var i = 0; i < images.length; i++) {
 			(function(index) {
-				images[i].addEventListener('click',function() {
+				var trigger = document.createElement('li');
+				trigger.className = 'slider-trigger';
+				trigger.addEventListener('click', function() {
+					slideTo(images,index);
+				});
+				images[i].addEventListener('click', function() {
 					slideNext(images,index);
 				});
+				nav.appendChild(trigger);
 			})(i);
 		}
 	}
