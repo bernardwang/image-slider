f23b75f8aca5b7ca28fde586d49779138b3a0272
bw-vbnm <bwang19@illinois.edu>
2015-11-25 15:45:40 -0500

    separated nav list

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var SLIDER_CLASS = 'slider';
-var IMAGE_CLASS = 'slider-image';
-var IMAGE_SHOW_CLASS = 'slider-image show';
+var IMAGE_CLASS = 'gallery-image';
+var IMAGE_SHOW_CLASS = 'gallery-image show';
+var TRIGGER_CLASS = 'nav-trigger';
 
 
 var slideTo = function(images, index) {
@@ -24,23 +25,42 @@ var slideNext = function(images, index) {
  *	TODO: more robust, less assumptions
  */
 var init = function() {
-	var sliders = document.getElementsByClassName('slider');
-	for(var sliderIndex = 0; sliderIndex < sliders.length; sliderIndex++) {
-		var slider = sliders[sliderIndex];
+	var sliders = document.getElementsByClassName(SLIDER_CLASS);
+	for(var slideIndex = 0; slideIndex < sliders.length; slideIndex++) {
+		var slider = sliders[slideIndex];
 		var images = slider.children[0].children; // hardcoded, FIX LATER
-		var nav = slider.children[1].children[0]; // hardcoded, FIX LATER
-
+		
+		var nav = slider.children[1]; // hardcoded, FIX LATER
+		var navSpacing = nav.dataset.spacing; // get spacing from data attribute
+		var navLeft = nav.children[0];	// hardcoded, FIX LATER
+		var navRight = nav.children[2];	// hardcoded, FIX LATER
+		
+		if(navSpacing < 0 || navSpacing > images.length) {
+			console.log('Spacing attribute on slider is invalid');
+			navSpacing = images.length;
+		}
+		
 		for(var i = 0; i < images.length; i++) {
 			(function(index) {
+				// Create nav trigger
 				var trigger = document.createElement('li');
-				trigger.className = 'slider-trigger';
+				trigger.className = TRIGGER_CLASS;
 				trigger.addEventListener('click', function() {
 					slideTo(images,index);
 				});
+				
+				// Separate nav trigger lists
+				if(index < navSpacing){
+					navLeft.appendChild(trigger);
+				}
+				else{
+					navRight.appendChild(trigger);	
+				}
+				
+				// Next image on click
 				images[i].addEventListener('click', function() {
 					slideNext(images,index);
 				});
-				nav.appendChild(trigger);
 			})(i);
 		}
 	}
